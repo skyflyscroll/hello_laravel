4b985857ea9216515e8097bc1e036ab73664cb56
Yuki <1209427779@qq.com>
2018-05-25 20:25:02 +0800

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,9 @@
+
+@extends('layouts.default')
+
+@section('title','关于君')
+
+
+@section('content')
+  <h1>关于页</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.default')
+
+@section('title','主页君')
+
+@section('content')
+  <h1>主页</h1>
+@stop
